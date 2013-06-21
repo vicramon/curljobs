4e94e14aac88672ab5d252d381cb59cda6072efd
Vic Ramon <WkdWMlFHaGhjMmh5YjJOclpYUXVZMjl0+vicramon@WkdWMlFHaGhjMmh5YjJOclpYUXVZMjl0>
2013-06-20 20:05:10 -0400

    Make assets compile in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Devjobs::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.assets.compile
+  config.assets.compile = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
